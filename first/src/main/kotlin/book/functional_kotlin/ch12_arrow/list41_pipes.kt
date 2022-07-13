240a8283953146bbfb005d2c1f513ff757600e86
curt <curt.k@kakaomobility.com>
2022-07-13 17:51:51 +0900

    minor

--- first/src/main/kotlin/book/functional_kotlin/ch12_arrow/list41_pipes.kt
+++
@@ -1,8 +0,0 @@
-package book.functional_kotlin.ch12_arrow
-
-//import arrow.syntax.function.pipe
-
-fun main(args: Array<String>) {
-    val strong: (String)->String = {body -> "<strong>$body</strong>"}
-    //"From a pipe".pipe(strong).pipe(::println)
-}
--- first/src/main/kotlin/suspend_test.kt
+++ first/src/main/kotlin/suspend_test.kt
@@ -1,28 +1,17 @@
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 
-interface Dao {
-    suspend fun getValue(key: String) : String
+
+suspend fun getValue1(key: String): String {
+    return "sync $key"
 }
 
-class SyncDao : Dao{
-    override suspend fun getValue(key: String): String {
-        return "sync $key"
-    }
-}
-class AsyncDao : Dao {
-    override suspend fun getValue(key: String): String {
-        delay(1)
-        return "async $key"
-    }
+suspend fun getValue2(key: String): String {
+    delay(1)
+    return "async $key"
 }
 
-fun main(args : Array<String>){
-    val syncDao = SyncDao()
-    val asyncDao = AsyncDao()
-
-    runBlocking {
-        println(syncDao.getValue("myid"))
-        println(asyncDao.getValue("myid"))
+fun main(args: Array<String>) = runBlocking {
+        println(getValue1("myid"))
+        println(getValue2("myid"))
     }
-}
